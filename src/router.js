48bff4e162330f5162137297d74749fb590110ce
陈泽 <18815288453@163.com>
2019-08-23 11:34:04 +0800

    hook案例

--- src/router.js
+++ src/router.js
@@ -17,6 +17,13 @@ export default {
                     cb(null, require("../page/b").default)
                 }, "home2")
             }
+        },{
+            path: `/root/hookdemo`,//首页
+            getComponent: (nextState, cb) => {
+                require.ensure([], (require) => {
+                    cb(null, require("../page/hook/HookA").default)
+                }, "home2")
+            }
         },
         {
             path: `/*`,//404页面，切记要放在路由的最后
